218d0fe960585057c80d4e5269efe941c72bd1fe
chiyz <3004346919@qq.com>
2019-07-04 18:14:15 +0800

    feat：添加悬浮球，控制网页的前进后退

--- MyJY/Class/Web/View/MyWebView.swift
+++ MyJY/Class/Web/View/MyWebView.swift
@@ -23,15 +23,111 @@ class MyWebView: UIView {
 		let webView = WKWebView(frame: self.bounds)
 		return webView
 	}()
+	
+	lazy var backButton: UIButton = {
+		let button = UIButton(type: .custom)
+		button.setImage(UIImage(named: "back"), for: .normal)
+		button.addTarget(self, action: #selector(backButtonClick(_:)), for: .touchUpInside)
+		return button
+	}()
+	
+	lazy var forwardButton: UIButton = {
+		let button = UIButton(type: .custom)
+		button.setImage(UIImage(named: "forward"), for: .normal)
+		button.addTarget(self, action: #selector(forwardButtonClick(_:)), for: .touchUpInside)
+		return button
+	}()
+	
+	lazy var contenView: UIView = {
+		let view = UIView(frame: CGRect(x: 0, y: self.frame.height*0.5, width: 100, height: 40))
+		view.backgroundColor = UIColor.lightGray
+		view.layer.cornerRadius = 5
+		view.alpha = 0.5
+		self.backButton.frame = CGRect(x: 0, y: 0, width: 50, height: 40)
+		self.forwardButton.frame = CGRect(x: 50, y: 0, width: 50, height: 40)
+		view.addSubview(self.backButton)
+		view.addSubview(self.forwardButton)
+		return view
+	}()
 
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		
 		self.addSubview(self.webView)
+		self.setupView()
+	}
+	
+	/// 初始化悬浮球
+	func setupView() -> Void {
+		// 添加拖拽事件
+//		UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
+		let pan1 = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+		self.backButton.addGestureRecognizer(pan1)
+		
+		let pan2 = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+		self.forwardButton.addGestureRecognizer(pan2)
+		
+		self.addSubview(self.contenView)
+	}
+	
+	@objc func handlePanGesture(_ p:UIPanGestureRecognizer) {
+		
+		let point = p.location(in: self)
+		if p.state == .began {
+			self.contenView.alpha = 1
+			
+		} else if p.state == .changed {
+			self.contenView.center = CGPoint(x: point.x, y: point.y)
+		} else if p.state == .ended || p.state == .cancelled {
+			self.contenView.alpha = 0.5
+			
+			let left = abs(point.x)
+			let right = abs(self.frame.width - left)
+			
+			var minSpace:CGFloat = 0.0
+			minSpace = min(left, right)
+			
+			var newCenter = CGPoint.zero
+			var targetY:CGFloat = 0
+			if point.y < 100 {
+				targetY = 100
+			} else if point.y > (self.frame.height - 100) {
+				targetY = self.frame.height - 100
+			} else {
+				targetY = point.y
+			}
+			
+			
+			if minSpace == left {
+				newCenter = CGPoint(x: 50, y: targetY)
+			} else {
+				newCenter = CGPoint(x: self.frame.width - 50, y: targetY)
+			}
+			
+			UIView.animate(withDuration: 0.25, animations: {
+				self.contenView.center = newCenter
+			}, completion: nil)
+			
+		} else {
+			print("pan State = \(p.state)")
+		}
+		
 	}
 	
 	func reloadWebView() -> Void {
 		self.webView.reload()
+	}
+	
+	@objc func backButtonClick(_ sender:UIButton) {
+		if self.webView.canGoBack {
+			self.webView.goBack()
+		}
+	}
+	
+	@objc func forwardButtonClick(_ sender:UIButton) {
+		if self.webView.canGoForward {
+			self.webView.goForward()
+		}
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
@@ -41,6 +137,7 @@ class MyWebView: UIView {
 	override func layoutSubviews() {
 		super.layoutSubviews()
 		self.webView.frame = self.frame
+		self.contenView.center = CGPoint(x: 50, y: self.frame.height * 0.5)
 	}
 	
 }
